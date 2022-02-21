48f687772735fce18d17a320e810dabaf0bde2a2
ggroce <ggrocedev@gmail.com>
2022-02-20 20:42:23 -0600

    added web3 contract interface for watching mim cauldron value

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-import express from 'express';
+require("dotenv").config();
+const express = require("express");
 
-// import cors from 'cors';
-
+const contract = require("./services/getMimAmount");
 const app = express();
 const port = process.env.PORT || 38000;
+const db = require("knex")({
+  client: "pg",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+  },
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,3 +17,11 @@ app.use(express.urlencoded({ extended: true }));
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+setInterval(async () => {
+  let mimBalance = await contract.askBentoBoxBalance();
+  console.log(
+    new Date().toLocaleString(),
+    `-- Mim balance in cauldron: ${mimBalance}`
+  );
+}, 5000);
--- services/getMimAmount.js
+++ services/getMimAmount.js
@@ -1,12 +1,38 @@
-import axios from 'axios';
-import { ethers } from 'ethers';
+const ethers = require("ethers");
+const cauldronABI = require("./cauldronV2ABI.json");
+const bentoBoxABI = require("./bentoBoxABI.json");
 
-import ETHERSCAN_ENDPOINT from '.././config/constants';
-import ETH_MIM_ADDRESS from '.././config/constants';
-import ETH_CAULDRONV2_ADDRESS from '.././config/constants';
-import ALCHEMY_ENDPOINT from '.././config/constants';
+const {
+  ETH_MIM_ADDRESS,
+  ETH_CAULDRONV2_ADDRESS,
+  ETH_BENTOBOX_ADDRESS,
+} = require("../config/constants");
 
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 
-export async function askCauldronForBalance() {}
+const provider = new ethers.getDefaultProvider("homestead", {
+  etherscan: ETHERSCAN_API_KEY,
+  alchemy: ALCHEMY_API_KEY,
+});
+
+const bentoBoxContract = new ethers.Contract(
+  ETH_BENTOBOX_ADDRESS,
+  bentoBoxABI,
+  provider
+);
+
+async function askBentoBoxBalance() {
+  const mimBalanceHex = await bentoBoxContract.balanceOf(
+    ETH_MIM_ADDRESS,
+    ETH_CAULDRONV2_ADDRESS
+  );
+
+  const mimBalance = ethers.utils.formatEther(mimBalanceHex.toString(), "wei");
+
+  return mimBalance;
+}
+
+module.exports = {
+  askBentoBoxBalance: askBentoBoxBalance,
+};
